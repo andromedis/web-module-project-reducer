7c1b4ca18b0bcf6cf7a640eb179dbed9c1191f48
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-03-15 21:24:51 +0000

    add one action connected

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 import reducer, { initialState } from './reducers';
-
+import { addOne } from './actions';
 
 function App() {
   const [ state, dispatch ] = useReducer(reducer, initialState);
@@ -34,7 +34,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton value={1} onClick={() =>  dispatch(addOne())}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
